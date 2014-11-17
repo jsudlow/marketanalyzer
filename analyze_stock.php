22299651548bde99366b7fb4f304906b471aef73
Jonathan Sudlow <jsudlow@gmail.com>
2014-11-16 21:17:41 -0600

    we now got charts!
    I cleaned up a few more bugs with the analyzer trying to get OHLC data
    from weekend and holiday days. If this happens I just reset the
    requested day back to the most recent trading day. I included the
    highcharts and highstock frameworks for charting and stock data
    visualization. An example is now showing up in the reports. Actual data
    that reflects that stock will start popping up in the next update.

--- analyze_stock.php
+++ analyze_stock.php
@@ -1,36 +1,39 @@
 <html>
 <head>
 	<title>MarketAnalyzer</title>
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+	<script src="js/highstock.js"></script>
+	<script src="js/modules/exporting.js"></script>
 </head>
 <body>
-<h1>Market Analyzer</h1>
+	<h1>Market Analyzer</h1>
 
-<form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-	Ticker Symbol: <input type='text' name="ticker_name" /><br />
-	<input type='submit' name='submit' value='Analyze Stock' />
-</form>
+	<form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+		Ticker Symbol: <input type='text' name="ticker_name" /><br />
+		<input type='submit' name='submit' value='Analyze Stock' />
+	</form>
 </body>
 </html>
 <?php
-	include('core_classes.php');
+include('core_classes.php');
 
-	$server = new ServerProperties;
+$server = new ServerProperties;
 
 
 	//If the page is in post mode (a ticker symbol was entered)
-	if ($server->is_post()) {
+if ($server->is_post()) {
 		//If a ticker symbol was entered correctly
-		$post = new PostAccess;
+	$post = new PostAccess;
 		//run analyze on the given ticker symbol
-		$analyzer = new Analyzer;
-		$ticker = $post->get_ticker_symbol();
+	$analyzer = new Analyzer;
+	$ticker = $post->get_ticker_symbol();
 
-		if($ticker != 'FAIL') {
-			$analyzer->analyze($ticker);
-		}
-		else {
-			echo 'ticker is invalid';
-		}
-		
+	if($ticker != 'FAIL') {
+		$analyzer->analyze($ticker);
 	}
-	?>
+	else {
+		echo 'ticker is invalid';
+	}
+	
+}
+?>
--- core_classes.php
+++ core_classes.php
@@ -47,12 +47,12 @@ class Analyzer {
 
 
 	function __construct() {
-       $this->today = date('Y-m-d');
-       $this->hour_trading_day_ends = 15;
+		$this->today = date('Y-m-d');
+		$this->hour_trading_day_ends = 15;
 
        //Lets get one single connection to the database, so we dont constantly re connect to the same thing.
-       $this->db_connection = $this->db_connect();
-    }
+		$this->db_connection = $this->db_connect();
+	}
 
 	public function db_connect() {
 		$username = "root";
@@ -70,57 +70,75 @@ class Analyzer {
 	}
 
 	public function analyze($ticker_symbol) {
+
+		$date = new DateTime($this->today);
 		
-		echo "Analyzing: " . "<b>$ticker_symbol</b>" . "<br />";
-		$sixdaysago = $this->days_ago(6);
-		echo "Analysis Report for <b>" . $ticker_symbol . "</b> for the date of <b>" . $this->today . "</b><br />";
+		
+    	//Lets check the start date to see if its on a weekend or holiday
+    	//If it is we will reverse the day to the last known valid trading day
+		if($this->check_weekend_and_holiday_dates($date)) {
+			echo 'start date has popped for weekend use';
+			$date = $this->reverse_date_to_last_trading_day($date->format('Y-m-d'));
+		}
+		$date = $date->format('Y-m-d');
+		
+		echo "Analyzing: " . "<b>$ticker_symbol</b>" . 
+		"<br /> Analysis Report for <b>" . $ticker_symbol . "</b> for the date of <b>" . $date . "</b><br />";
+		
 		if (date('H') <= $this->hour_trading_day_ends) { 
-		echo 'Trading day has not concluded - unable to show hi,lo or close! <br />';
-		 }
-		 else {
-		 	$this->display_daychart($ticker_symbol, $this->today);
-		 }
-		//Start analyzing the stock by displaying a day chart and a range to start to get a trend established		
+			echo 'Trading day has not concluded - unable to show hi,lo or close! <br />';
+		}
+		else {
+
+			$this->display_daychart($ticker_symbol, $date);
+		}
 		
-		$this->display_chart_daterange($ticker_symbol,$sixdaysago,$this->today);
+		//Start analyzing the stock by displaying a day chart and a range to start to get a trend established		
+		$sixdaysago = $this->days_ago(6);
+		$this->display_chart_daterange($ticker_symbol,$sixdaysago,$date);
 
 		//Lets get a trend!
-		$this->calculate_trend($ticker_symbol,$sixdaysago,$this->today);
+		$this->calculate_trend($ticker_symbol,$sixdaysago,$date);
+
+		//Lets display a chart
+		$this->generate_chart($ticker_symbol);
 	}
 
 	public function read_yahoo_single_ohlc($ticker_symbol,$date) {
-			$date_begin_ind = explode("-",$date);
-			$begin_month = $date_begin_ind[1] -1;
-        	$begin_day = $date_begin_ind[2];
-        	$begin_year = $date_begin_ind[0];
-			$url = "http://ichart.yahoo.com/table.csv?s=$ticker_symbol&a=$begin_month&b=$begin_day&c=$begin_year&d=$begin_month&e=$begin_day&f=$begin_year&g=d";
+
+
+		$date_begin_ind = explode("-",$date);
+		$begin_month = $date_begin_ind[1] -1;
+		$begin_day = $date_begin_ind[2];
+		$begin_year = $date_begin_ind[0];
+		$url = "http://ichart.yahoo.com/table.csv?s=$ticker_symbol&a=$begin_month&b=$begin_day&c=$begin_year&d=$begin_month&e=$begin_day&f=$begin_year&g=d";
 			//echo $url;
-	    	$file_handle = @fopen($url, "r");
+		$file_handle = @fopen($url, "r");
 			if ( $file_handle == FALSE ) { // If the URL can't be opened
-				echo "Cannot get data from Yahoo! Finance. The following URL is not accessible, $url";
+			echo "Cannot get data from Yahoo! Finance. The following URL is not accessible, $url";
 			return -1; // ERROR
+		}
+		$hi="";
+		$low="";
+		$open="";
+		$close="";
+		$row = 0;
+		while (!feof($file_handle) ) {
+			$line_of_text = fgetcsv($file_handle, 1024);
+			if($row==0) {
+				$row++;
+				continue;
 			}
-			$hi="";
-			$low="";
-			$open="";
-			$close="";
-			$row = 0;
-	    	while (!feof($file_handle) ) {
-         		$line_of_text = fgetcsv($file_handle, 1024);
-         		if($row==0) {
-         			$row++;
-         			continue;
-         		}
-	         	if($line_of_text[0] != "") {
-    	     		$open=$line_of_text[1];
-        	 	    $hi = $line_of_text[2];
-         		    $low= $line_of_text[3];
-         	   		$close=$line_of_text[4];    
-         		}    
-         	}
-         	$return_array = array("open"=>$open,"hi"=>$hi,"low"=>$low,"close"=>$close);
-         	
-         	return $return_array;
+			if($line_of_text[0] != "") {
+				$open=$line_of_text[1];
+				$hi = $line_of_text[2];
+				$low= $line_of_text[3];
+				$close=$line_of_text[4];    
+			}    
+		}
+		$return_array = array("open"=>$open,"hi"=>$hi,"low"=>$low,"close"=>$close);
+		
+		return $return_array;
 
 	}
 	public function validate_yahoo_single_ohlc($ticker_symbol,$ohlc_array,$date) {
@@ -130,23 +148,24 @@ class Analyzer {
 		$hi = $ohlc_array['hi'];
 		$low = $ohlc_array['low'];
 		$close = $ohlc_array['close'];
-    	
-    	if($open != "" && $hi != "" && $low != "" && $close != "") { 
-    		echo "snap is good<br />";
-   			$this->capture_snapshot($ticker_symbol,$date,$hi,$low,$open,$close);
-   		}
-   		else {
-   			die("snapshot invalid; ticker symbol is most likly not valid");
-   		}
+		
+		if($open != "" && $hi != "" && $low != "" && $close != "") { 
+			echo "snap is good<br />";
+			$this->capture_snapshot($ticker_symbol,$date,$hi,$low,$open,$close);
+		}
+		else {
+			die("snapshot invalid; ticker symbol is most likly not valid");
+		}
 
 	}
 	public function check_snapshot($ticker_symbol,$date) {
+
 		echo "Checking for snap in db <br />";
 		$conn = $this->db_connection;
 		$sql = "select * from stock_snapshot where ticker_symbol = '$ticker_symbol' and date_captured = '$date';";
 		//echo $sql;
 		$result = mysqli_query($conn,$sql);
-	
+		
 		$num_rows = mysqli_num_rows($result);
 		
 		if($num_rows == 0) {
@@ -155,7 +174,7 @@ class Analyzer {
 			$this->validate_yahoo_single_ohlc($ticker_symbol,$ohlc_array,$date);		
 		}
 		else {
-				echo "we already got the snap! <br />";
+			echo "we already got the snap! <br />";
 		}
 		
 
@@ -184,81 +203,81 @@ class Analyzer {
 		$sql = "insert into stock_snapshot values(DEFAULT,'$ticker_symbol','$date',$hi,$lo,$open,$close);";
 		//echo $sql;
 		mysqli_query($conn,$sql);
-    }
-    public function calculate_price_movement($ticker_symbol,$date) {
-    	$conn = $this->db_connection;
+	}
+	public function calculate_price_movement($ticker_symbol,$date) {
+		$conn = $this->db_connection;
 		$sql = "select * from stock_snapshot where ticker_symbol = '$ticker_symbol' and date_captured = '$date';";
 		$result = mysqli_query($conn,$sql);
 		$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
 		return $row['open'] - $row['close'];
-    }
-    public function calculate_percent_difference($num1,$num2) {
-    	$v1 = $num1;
-    	$v2 = $num2;
+	}
+	public function calculate_percent_difference($num1,$num2) {
+		$v1 = $num1;
+		$v2 = $num2;
 
-    	$percent_difference = abs($v1 - $v2) / (($v1 + $v2)/2) * 100;
-    	return number_format((float)$percent_difference, 2, '.', '');
-    }
+		$percent_difference = abs($v1 - $v2) / (($v1 + $v2)/2) * 100;
+		return number_format((float)$percent_difference, 2, '.', '');
+	}
 
-    public function reverse_date_to_last_trading_day($date) {
-    	$day_of_week = date('l',strtotime($date));
+	public function reverse_date_to_last_trading_day($date) {
+		$day_of_week = date('l',strtotime($date));
 		if($day_of_week == 'Sunday') {
 			return new DateTime(date('Y-m-d', strtotime('-2 day', strtotime($date))));
 
 		} 
 		if($day_of_week == 'Saturday') {
 			return new DateTime(date('Y-m-d', strtotime('-1 day', strtotime($date))));
-    	}
-    	
-    }
-    public function calculate_trend($ticker_symbol,$date_begin,$date_end) {
-    	$start_date = new DateTime($date_begin);
-    	$end_date = new DateTime($date_end);
-    	    	
+		}
+		
+	}
+	public function calculate_trend($ticker_symbol,$date_begin,$date_end) {
+		$start_date = new DateTime($date_begin);
+		$end_date = new DateTime($date_end);
+		
     	//Lets check the start date to see if its on a weekend or holiday
     	//If it is we will reverse the day to the last known valid trading day
-    	if($this->check_weekend_and_holiday_dates($start_date)) {
-    		echo 'start date has popped for weekend use';
-    		$start_date = $this->reverse_date_to_last_trading_day($start_date->format('Y-m-d'));
-    	}
-    	
+		if($this->check_weekend_and_holiday_dates($start_date)) {
+			echo 'start date has popped for weekend use';
+			$start_date = $this->reverse_date_to_last_trading_day($start_date->format('Y-m-d'));
+		}
+		
         //Lets do the same checks with the end date
-        if($this->check_weekend_and_holiday_dates($end_date)) {
-        	echo 'end date has popped for weekend use';
-    		$end_date = $this->reverse_date_to_last_trading_day($end_date->format('Y-m-d'));
-    	}
+		if($this->check_weekend_and_holiday_dates($end_date)) {
+			echo 'end date has popped for weekend use';
+			$end_date = $this->reverse_date_to_last_trading_day($end_date->format('Y-m-d'));
+		}
 
     	//Now we have a valid day lets check to see if we got the snap in our database
-    	$this->check_snapshot($ticker_symbol,$start_date->format('Y-m-d'));
-        $this->check_snapshot($ticker_symbol,$end_date->format('Y-m-d'));
+		$this->check_snapshot($ticker_symbol,$start_date->format('Y-m-d'));
+		$this->check_snapshot($ticker_symbol,$end_date->format('Y-m-d'));
 
         //Now that we got ours snap in our db we can assign the accurate open and closes 
-        $start = $this->get_close_by_date($ticker_symbol,$start_date->format('Y-m-d'));
-        $end = $this->get_close_by_date($ticker_symbol,$end_date->format('Y-m-d'));
+		$start = $this->get_close_by_date($ticker_symbol,$start_date->format('Y-m-d'));
+		$end = $this->get_close_by_date($ticker_symbol,$end_date->format('Y-m-d'));
 
-        
+		
         //Now we can print the trend report
-        echo "Start: (" . $start_date->format('Y-m-d') . ") <b>" . $start . "</b><br />";
-        echo "End: (" . $end_date->format('Y-m-d') . ") <b>" . $end . "</b><br />";
+		echo "Start: (" . $start_date->format('Y-m-d') . ") <b>" . $start . "</b><br />";
+		echo "End: (" . $end_date->format('Y-m-d') . ") <b>" . $end . "</b><br />";
         //baseline trend read
-        if($start > $end) {
-        	
-        	echo "Trend is <span style='color:red;'>negative</span>.<br /> Down from six days ago by " . $this->calculate_percent_difference($start,$end) . " %";
-        }
-        else {
-        	echo "Trend is <span style='color:green';>postive</span>.<br /> Up from six days ago by " . $this->calculate_percent_difference($start,$end) . " %";
-        }
+		if($start > $end) {
+			
+			echo "Trend is <span style='color:red;'>negative</span>.<br /> Down from six days ago by " . $this->calculate_percent_difference($start,$end) . " %";
+		}
+		else {
+			echo "Trend is <span style='color:green';>postive</span>.<br /> Up from six days ago by " . $this->calculate_percent_difference($start,$end) . " %";
+		}
 		
 		
-    }
+	}
 
-    public function get_close_by_date($ticker_symbol,$date) {
-    	$conn = $this->db_connection;
+	public function get_close_by_date($ticker_symbol,$date) {
+		$conn = $this->db_connection;
 		$sql = "select * from stock_snapshot where ticker_symbol = '$ticker_symbol' and date_captured = '$date';";
 		$result = mysqli_query($conn,$sql);
 		$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
 		return $row['close'];
-    }
+	}
 
 	public function display_chart_daterange($ticker_symbol,$date_begin,$date_end) {
 		//declare a dates array to hold all the dates we will be looking at
@@ -270,7 +289,7 @@ class Analyzer {
 		echo 'Date Begin: ' . $date_begin . "<br />";
 		echo 'Date End: ' . $date_end . "<br />";  
 		echo "</div><br />";
-	
+		
 		for($i=$date_begin_i;$i<=$date_end_e;$i->modify('+1 day')) {
 
 			//First lets check to make sure the day we are attempting to pull the quotes
@@ -286,9 +305,11 @@ class Analyzer {
 			//our generic pull function to display the high, low, open and close for the particular day we are investigating
 			$this->display_hloc_daily($ticker_symbol, $i->format('Y-m-d'));
 			
+			
 		}
+
 		
-		}
+	}
 	public function check_weekend_and_holiday_dates($date) {
 		$day_of_week = date('l',strtotime($date->format('Y-m-d')));
 		if($day_of_week == 'Sunday' || $day_of_week == 'Saturday') {
@@ -300,8 +321,73 @@ class Analyzer {
 
 	}
 
-		
-	}
+	public function generate_chart($ticker_symbol) {
+		echo "
+
+		<script type='text/javascript'>
+		$(function () {
+			
+
+        // create the chart
+			window.chart = new Highcharts.StockChart({
+				chart : {
+					renderTo : 'container'
+				},
+
+				rangeSelector : {
+					inputEnabled: $('#container').width() > 480,
+					selected : 2
+				},
+
+				title : {
+					text : '$ticker_symbol'
+				},
+
+				series : [{
+					type : 'ohlc',
+					name : '$ticker_symbol Stock Price',
+					data : [
+					/* May 2006 */
+					[1147651200000,67.37,68.38,67.12,67.79],
+					[1147737600000,68.10,68.25,64.75,64.98],
+					[1147824000000,64.70,65.70,64.07,65.26],
+					[1147910400000,65.68,66.26,63.12,63.18],
+					[1147996800000,63.26,64.88,62.82,64.51],
+					[1148256000000,63.87,63.99,62.77,63.38],
+					[1148342400000,64.86,65.19,63.00,63.15],
+					[1148428800000,62.99,63.65,61.56,63.34],
+					[1148515200000,64.26,64.45,63.29,64.33],
+					[1148601600000,64.31,64.56,63.14,63.55],
+					[1148947200000,63.29,63.30,61.22,61.22],
+					[1149033600000,61.76,61.79,58.69,59.77]],
+					
+					
+				}]
+			});
+});
+
+
+</script>
+
+
+
+
+
+<div id='container' style='height: 400px; min-width: 310px'></div>
+
+
+";
+
+
+}	
+public function generate_highstock_data_200_day($ticker_symbol,$date) {
+		//Capture the data into the database
+
+		//Construct the 2 dim array of data for the OHLC
+
+		//Return the data 
+}
+}
 //end of analyzer class
 
 
